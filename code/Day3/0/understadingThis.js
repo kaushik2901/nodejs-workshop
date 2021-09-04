a912928fa6402af3c636ddfa6fb0e94b3012b50a
kaushik2901 <kaushikjadav602@gmail.com>
2021-09-04 13:51:53 +0530

    push

--- code/Day3/0/understadingThis.js
+++ code/Day3/0/understadingThis.js
@@ -1,45 +1,45 @@
-console.log(module);
-console.log(this == module.exports);
+// console.log(module);
+// console.log(this == module.exports);
 
-this.data = "attaching data to our module!";
-console.log(this.data);
-console.log(module.exports);
+// this.data = "attaching data to our module!";
+// console.log(this.data);
+// console.log(module.exports);
 
-module.exports.foo = 5;
+// module.exports.foo = 5;
 
-console.log(this);
+// console.log(this);
 
-const counter = {
-  count: 2,
-  next: function () {
-    console.log("counter next function this -> ", this);
-    return this.count + 1;
-  },
-};
+// const counter = {
+//   count: 2,
+//   next: function () {
+//     console.log("counter next function this -> ", this);
+//     return this.count + 1;
+//   },
+// };
 
-console.log(counter.next());
+// console.log(counter.next());
 
-const counterArrow = {
-  count: 2,
-  next: () => {
-    console.log(
-      "counterArrow this is module.exports ?",
-      this == module.exports
-    );
-    return this.count + 1;
-  },
-};
+// const counterArrow = {
+//   count: 2,
+//   next: () => {
+//     console.log(
+//       "counterArrow this is module.exports ?",
+//       this == module.exports
+//     );
+//     return this.count + 1;
+//   },
+// };
 
-console.log(counterArrow.next());
+// console.log(counterArrow.next());
 
-let obj = {
-  func1: function () {
-    console.log("func1 -> ", this);
-  },
-  func2: () => {
-    console.log("func2 -> ", this);
-  },
-};
+// let obj = {
+//   func1: function () {
+//     console.log("func1 -> ", this);
+//   },
+//   func2: () => {
+//     console.log("func2 -> ", this);
+//   },
+// };
 
-obj.func1(); // obj is left of the dot, so this is obj
-obj.func2();
+// obj.func1();
+// obj.func2();
