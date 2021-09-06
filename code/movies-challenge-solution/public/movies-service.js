ac68d8b44f0de46790120d719a929048eca2b891
kaushik2901 <kaushikjadav602@gmail.com>
2021-09-06 18:45:58 +0530

    solution

---
+++ code/movies-challenge-solution/public/movies-service.js
@@ -0,0 +1,71 @@
+const url = "http://localhost:3000/api/movies";
+
+export default {
+  getAllMovies(callback) {
+    // TODO: Fetch from api
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => callback(data));
+  },
+
+  getMovie(id, callback) {
+    // TODO: Fetch from api
+    fetch(`${url}/${id}`)
+      .then((res) => res.json())
+      .then((data) => callback(data));
+  },
+
+  addMovie(data, callback) {
+    // TODO: Implement following method with fetch API
+    // TODO: On successful API call execute callback
+    fetch(`${url}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => callback(data));
+  },
+
+  updateMovie(id, data, callback) {
+    // TODO: Implement following method with fetch API
+    // TODO: On successful API call execute callback
+    fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => callback(data));
+  },
+
+  deleteMovie(id, callback = () => {}) {
+    // TODO: Implement following method with fetch API
+    // TODO: On successful API call execute callback
+    fetch(`${url}/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => callback(data));
+  },
+
+  incrementLike(id, callback = () => {}) {
+    // TODO: Implement following method with fetch API
+    // TODO: On successful API call execute callback
+    fetch(`${url}/${id}/increment-like`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => callback(data));
+  },
+};
