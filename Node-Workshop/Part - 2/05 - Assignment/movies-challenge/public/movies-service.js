519f0e3b8823505dc71a29fe480997eca71a8289
Rohan <40306359+Rohan175@users.noreply.github.com>
2021-10-15 17:01:39 +0000

    part 2

---
+++ Node-Workshop/Part - 2/05 - Assignment/movies-challenge/public/movies-service.js
@@ -0,0 +1,31 @@
+const url = "http://localhost:3000/api/movies";
+
+export default {
+  getAllMovies(callback) {
+    // TODO: Implement following method with fetch API
+  },
+
+  getMovie(id, callback) {
+    // TODO: Implement following method with fetch API
+  },
+
+  addMovie(data, callback) {
+    // TODO: Implement following method with fetch API
+    // TODO: On successful API call execute callback
+  },
+
+  updateMovie(id, data, callback) {
+    // TODO: Implement following method with fetch API
+    // TODO: On successful API call execute callback
+  },
+
+  deleteMovie(id, callback = () => {}) {
+    // TODO: Implement following method with fetch API
+    // TODO: On successful API call execute callback
+  },
+
+  incrementLike(id, callback = () => {}) {
+    // TODO: Implement following method with fetch API
+    // TODO: On successful API call execute callback
+  },
+};
