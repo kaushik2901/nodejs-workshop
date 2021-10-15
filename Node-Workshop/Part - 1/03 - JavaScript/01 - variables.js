170464d3cede92ce6eea9d733ec88f44a9d6ced9
kaushik2901 <kaushikjadav602@gmail.com>
2021-10-15 22:57:45 +0530

    changes

--- Node-Workshop/Part - 1/03 - JavaScript/01 - variables.js
+++ Node-Workshop/Part - 1/03 - JavaScript/01 - variables.js
@@ -1,15 +1,37 @@
 // Declaration
 
-var x = 5;
-var y = 6;
-var z = x + y;
+var a = 5;
+var b = 6;
+var c = a + b;
 
-console.log("z = ", z);
+console.log("c = ", c);
 
 
-// 
-let x = "John Doe";
+// Three ways to declare variable
 
-let x = 0;
+var d = "Variable"; // Function scoped
+let e = "Variable"; // Block scoped
+const f = "Variable"; // Block scoped
 
-// SyntaxError: 'x' has already been declared
+
+var age = 23 // Numbers (integer)
+var pi = 3.14 // Numbers (float)
+var text = "init" // Text (strings)
+var operation = 1 + 2 + 3 // Operations
+var boolean = true // Boolean (True or false statements)
+var user = { firstName: "John", lastName: "Doe" } // Objects
+var u = undefined // undefined
+console.log(user.age); // undefined
+var x = null; // null
+
+
+// hoist
+console.log("unDeclaredVariable = ", unDeclaredVariable);
+var unDeclaredVariable = 10;
+
+// console.log("unDeclaredLetVariable = ", unDeclaredLetVariable);
+// let unDeclaredLetVariable = 10;
+
+// console.log("unDeclaredConstVariable = ", unDeclaredConstVariable);
+// let unDeclaredConstVariable = 10;
+
